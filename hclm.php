cb4edabb95866b5e5f2de128699b9b8370bbcc15
soniiix <quent.couzinet@gmail.com>
2025-04-17 15:26:21 +0200

    chore: rename shortcodes for consistency

--- hclm.php
+++ hclm.php
@@ -12,14 +12,14 @@ defined('ABSPATH') || exit;
 require_once plugin_dir_path(__FILE__) . 'includes/hooks/filters.php';
 require_once plugin_dir_path(__FILE__) . 'includes/hooks/actions.php';
 require_once plugin_dir_path(__FILE__) . 'includes/shortcodes/login-button.php';
-require_once plugin_dir_path(__FILE__) . 'includes/shortcodes/member-sign-up.php';
+require_once plugin_dir_path(__FILE__) . 'includes/shortcodes/registration.php';
 require_once plugin_dir_path(__FILE__) . 'includes/shortcodes/newsletters-summaries.php';
 require_once plugin_dir_path(__FILE__) . 'includes/shortcodes/login-form.php';
 
 // Save shortcodes
-add_shortcode('hclm_login_button', 'hclm_login_button_shortcode');
-add_shortcode('member_sign_up', 'hclm_member_sign_up');
-add_shortcode('newsletter_summaries', 'hclm_newsletters_summaries');
-add_shortcode('hclm_login_form', 'hclm_login_form_shortcode');
+add_shortcode('hclm_login_button', 'login_button_shortcode');
+add_shortcode('hclm_registration_form', 'registration_form_shortcode');
+add_shortcode('hclm_newsletter_summaries', 'newsletters_summaries_shortcode');
+add_shortcode('hclm_login_form', 'login_form_shortcode');
 
 ?>

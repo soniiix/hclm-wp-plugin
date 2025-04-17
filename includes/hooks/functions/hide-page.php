f6ec345204470b0182e49420db9181a368d7e90e
soniiix <quent.couzinet@gmail.com>
2025-04-17 17:05:58 +0200

    docs: add documentation to hide_page function

--- includes/hooks/functions/hide-page.php
+++ includes/hooks/functions/hide-page.php
@@ -1,5 +1,11 @@
 <?php 
 
+/**
+ * Redirects the user to the home page if he tries to access a specific page, based on his login status.
+ * 
+ * @param string $page_name The slug of the page to restrict.
+ * @param bool $logged_in Whether the user should be logged in to access the page.
+ */
 function hide_page($page_name, $logged_in) {
     $condition = $logged_in ? is_user_logged_in() : !is_user_logged_in();
     if ($condition && is_page($page_name)) {

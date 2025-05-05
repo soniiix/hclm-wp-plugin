9376ce5b99649c1a326d8c7a30f0e90b4ca1edb5
soniiix <quent.couzinet@gmail.com>
2025-05-05 11:06:40 +0200

    chore: remove useless meta boxes on fall visit form

--- includes/hooks/functions/admin/register-post-type.php
+++ includes/hooks/functions/admin/register-post-type.php
@@ -13,7 +13,7 @@ function register_visit_post_type() {
         'public' => true,                  
         'has_archive' => false, 
         'rewrite' => ['slug' => 'visites-automnales'],
-        'supports' => ['title', 'editor', 'thumbnail'],
+        'supports' => ['title', 'thumbnail'],
         'show_ui' => true,
         'show_in_rest' => false,
         'show_in_menu' => 'hclm-manage'
@@ -21,5 +21,10 @@ function register_visit_post_type() {
 }
 
 add_action('init', 'register_visit_post_type');
+
+// Remove astra settings meta box
+add_action('do_meta_boxes', function () {
+    remove_meta_box('astra_settings_meta_box', 'visite_automnale', 'normal');
+});
 
 ?>

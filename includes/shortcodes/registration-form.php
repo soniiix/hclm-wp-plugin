992bf783dcf5a7d2a3b6bf633adfc65ec8cb2b7a
soniiix <quent.couzinet@gmail.com>
2025-05-06 11:53:36 +0200

    chore: add autocomplete attribute to each form input

--- includes/shortcodes/registration-form.php
+++ includes/shortcodes/registration-form.php
@@ -16,12 +16,12 @@ function registration_form_shortcode() {
     ob_start();
     ?>
     <form method="post" class="hclm_form">
-        <p><label>Nom :</label><br><input type="text" name="member_lastname" required></p>
-        <p><label>Prénom :</label><br><input type="text" name="member_firstname" required></p>
+        <p><label>Nom :</label><br><input type="text" name="member_lastname" autocomplete="family-name" required></p>
+        <p><label>Prénom :</label><br><input type="text" name="member_firstname" autocomplete="given-name" required></p>
         <p><label>Adresse :</label><br><textarea rows="2" type="text" name="member_address" required></textarea></p>
-        <p><label>Téléphone :</label><br><input type="tel" name="member_phone" required></p>
-        <p><label>Email :</label><br><input type="email" name="member_email" required></p>
-        <p><label>Mot de passe :</label><br><input type="password" name="member_password" required></p>
+        <p><label>Téléphone :</label><br><input type="tel" name="member_phone" autocomplete="tel" required></p>
+        <p><label>Email :</label><br><input type="email" name="member_email" autocomplete="email" required></p>
+        <p><label>Mot de passe :</label><br><input type="password" name="member_password" autocomplete="new-password" required></p>
         <p><input type="submit" name="sign_up_submit" value="Créer mon compte"></p>
     </form>
     <?php

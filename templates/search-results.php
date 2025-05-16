f84ba5e213b100f5ee19bde0c1efd0ef219d284e
soniiix <quent.couzinet@gmail.com>
2025-05-16 09:26:23 +0200

    style: replace arrow with svg

--- templates/search-results.php
+++ templates/search-results.php
@@ -25,7 +25,11 @@ wp_enqueue_style('hclm-search-results-style', plugin_dir_url(__FILE__) . '../ass
                                 <h3 class="hclm-result-title"><?php the_title(); ?></h3>
                                 <p class="hclm-result-excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit.<?php //the_excerpt(); ?></p>
                             </div>
-                            <div class="hclm-result-arrow">→</div>
+                            <div class="hclm-result-arrow">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                                </svg>
+                            </div>
                         </a>
                     </li>
                 <?php endwhile; ?>
@@ -72,7 +76,11 @@ wp_enqueue_style('hclm-search-results-style', plugin_dir_url(__FILE__) . '../ass
                             <h3 class="hclm-result-title"><?php echo esc_html($item['title']); ?></h3>
                             <p class="hclm-result-excerpt">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                         </div>
-                        <div class="hclm-result-arrow">→</div>
+                        <div class="hclm-result-arrow">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                            </svg>
+                        </div>
                     </a>
                 </li>
             <?php } ?>

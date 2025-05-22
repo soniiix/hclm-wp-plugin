dbef514211bb542c3ac931f0f0ad8a95ccac325a
soniiix <quent.couzinet@gmail.com>
2025-05-22 10:36:58 +0200

    feat: add a custom post type for newsletters

--- includes/hooks/functions/admin/register-post-type.php
+++ includes/hooks/functions/admin/register-post-type.php
@@ -1,14 +1,16 @@
 <?php
 
 // Add a custom post type for fall visits
-
 function register_visit_post_type() {
     register_post_type('visite_automnale', [
         'labels' => [
             'name' => 'Visites automnales',
             'singular_name' => 'Visite automnale',
             'add_new_item' => 'Ajouter une visite automnale',
-            'edit_item' => 'Modifier la visite',
+            'edit_item' => 'Modifier la visite automnale',
+            'search_items' => 'Rechercher des visites automnales',
+            'not_found' => 'Aucune visite automnale trouvée',
+            'not_found_in_trash' => 'Aucune visite automnale dans la corbeille',
         ],
         'public' => true,                  
         'has_archive' => false, 
@@ -19,8 +21,30 @@ function register_visit_post_type() {
         'show_in_menu' => 'hclm-manage'
     ]);
 }
-
 add_action('init', 'register_visit_post_type');
+
+// Add a custom post type for newsletters
+function register_newsletter_post_type() {
+    register_post_type('bulletin', [
+        'labels' => [
+            'name' => 'Bulletins',
+            'singular_name' => 'Bulletin',
+            'add_new_item' => 'Ajouter un bulletin',
+            'edit_item' => 'Modifier le bulletin',
+            'search_items' => 'Rechercher des bulletins',
+            'not_found' => 'Aucun bulletin trouvé',
+            'not_found_in_trash' => 'Aucun bulletin dans la corbeille',
+        ],
+        'public' => true,
+        'has_archive' => false,
+        'rewrite' => ['slug' => 'bulletins'],
+        'supports' => ['title', 'thumbnail'],
+        'show_ui' => true,
+        'show_in_rest' => false,
+        'show_in_menu' => 'hclm-manage',
+    ]);
+}
+add_action('init', 'register_newsletter_post_type');
 
 // Remove astra settings meta box
 add_action('do_meta_boxes', function () {

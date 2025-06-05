cbfedfca7cd57fbcf335879122171f5ca56d2e7c
soniiix <quent.couzinet@gmail.com>
2025-06-05 11:03:48 +0200

    feat: add the association's status

--- includes/shortcodes/member-area.php
+++ includes/shortcodes/member-area.php
@@ -134,9 +134,23 @@ function member_area_shortcode() {
                 <div class="tab-card">
                     <p>Les statuts de l'association HCLM sont disponibles ci-dessous.</p>
                     <?php $status_pdf_url = wp_upload_dir()['baseurl'] . '/hclm/documents/statuts-hclm.pdf'; ?>
-                    <a href="<?php echo esc_url(plugin_dir_url(__FILE__) . '../../assets/documents/statuts-hclm.pdf'); ?>" class="btn-download" target="_blank" download>Télécharger</a>
-                    <div class="_df_button" source="<?php echo esc_url($status_pdf_url) ; ?>">
-                        Consulter le fichier PDF
+                    <div class="hclm-status-buttons-row">
+                        <a 
+                            href="<?php echo esc_url($status_pdf_url); ?>" 
+                            class="btn-download btn-download-with-icon"
+                            target="_blank" 
+                            download
+                            aria-label="Télécharger les statuts de l'association HCLM"
+                        >
+                            Télécharger 
+                            <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="20" width="20" xmlns="http://www.w3.org/2000/svg" style="margin-top: 1px;"><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path><path d="M7 11l5 5l5 -5"></path><path d="M12 4l0 12"></path></svg>
+                        </a>
+                        <div 
+                            class="_df_button" 
+                            source="<?php echo esc_url($status_pdf_url); ?>"
+                        >
+                            Consulter le fichier PDF
+                        </div>
                     </div>
                 </div>
             </section>
